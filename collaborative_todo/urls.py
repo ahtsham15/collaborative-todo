6369e6879bfc05610fa5aeebd39fbabc29e3c9ae
Ahtsham <ahtshamsandhu05@gmail.com>
2024-12-13 20:14:46 +0500

    task/views/taskView: two get api and one delete api done of task-do

--- collaborative_todo/urls.py
+++ collaborative_todo/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from tasks.views.userView import RegisterView, LoginView
 from tasks.views.taskListView import TaskListView, TaskListDetailView
-from tasks.views.taskView import TaskDoView
+from tasks.views.taskView import TaskDoView, TaskDoViewDetail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,5 +10,6 @@ urlpatterns = [
     path('api/login/', LoginView.as_view(), name='auth_login'),
     path('api/task-list/', TaskListView.as_view(), name='task_list'),
     path('api/task-list/<int:pk>/', TaskListDetailView.as_view(), name='task_list_detail'),
-    path('api/task-do/', TaskDoView.as_view(), name='task_do_view')
+    path('api/task-do/', TaskDoView.as_view(), name='task_do_view'),
+    path('api/task-do/<int:id>/', TaskDoViewDetail.as_view(), name='task_do_view_detail')
 ]
--- tasks/views/taskView.py
+++ tasks/views/taskView.py
@@ -2,6 +2,7 @@ from rest_framework.views import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from tasks.models.taskModel import Task
+from tasks.models.taskDoModel import TaskDo
 from tasks.models.userModel import User
 from tasks.serializers import TaskSerializer
 from tasks.serializers import TaskDoSerializer
@@ -47,3 +48,89 @@ class TaskDoView(APIView):
                 "status": "error",
                 "message": f"An error occurred: {str(e)}"
             }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+    def get(self, request):
+        if not request.user.is_authenticated:
+            return Response({
+                "status": "error",
+                "message": "Access token not provided or invalid"
+            }, status=status.HTTP_401_UNAUTHORIZED)
+        try:
+            user_id = request.user.id
+            try:
+                user = User.objects.get(id=user_id)
+            except User.DoesNotExist:
+                return Response({
+                    "status": "error",
+                    "message": "User not found"
+                },status=status.HTTP_404_NOT_FOUND)
+            tasks = TaskDo.objects.filter(user=user)
+            serializer = TaskDoSerializer(tasks,many=True)
+            return Response({
+                "status": "success",
+                "data": serializer.data
+            },status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({
+                "status": "error",
+                "message": f"An error occured: {str(e)}"
+            },status=status.HTTP_400_BAD_REQUEST)
+
+class TaskDoViewDetail(APIView):
+    def get(self,request,id):
+        if not request.user.is_authenticated:
+            return Response({
+                "status": "error",
+                "message": "Access token not provided or invalid"
+            },status=status.HTTP_401_UNAUTHORIZED)
+        try:
+            user_id = request.user.id
+            try:
+                user = User.objects.get(id=user_id)
+            except User.DoesNotExist:
+                return Response({
+                    "status": "error",
+                    "message": "User not found"
+                },status=status.HTTP_404_NOT_FOUND)
+            try:
+                task = TaskDo.objects.get(id=id)
+            except TaskDo.DoesNotExist:
+                return Response({
+                    "status": "error",
+                    "message": "Task not found"
+                },status=status.HTTP_404_NOT_FOUND)
+            serializer = TaskDoSerializer(task)
+            return Response({
+                "status": "success",
+                "data":serializer.data
+            },status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({
+                "status": "error",
+                "message": f"An error occured: {str(e)}"
+            },status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request,id):
+        if not request.user.is_authenticated:
+            return Response({
+                "status": "error",
+                "message": "Access token not provided or invalid"
+            },status=status.HTTP_401_UNAUTHORIZED)
+        try:
+            task = TaskDo.objects.get(id=id)
+            if task:
+                task.delete()
+                return Response({
+                    "status": "success",
+                    "message": "Task deleted successfully"
+                },status=status.HTTP_200_OK)
+            else:
+                return Response({
+                    "status": "error",
+                    "message": "Task not found"
+                },status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({
+                "status": "error",
+                "message": f"An error occured: {str(e)}"
+            },status=status.HTTP_400_BAD_REQUEST)
